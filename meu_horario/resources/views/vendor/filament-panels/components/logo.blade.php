16f74c7ad543dcab45c47ac6a20052b7d768fed5
Jorge Gouveia <jmvgouveia@gmail.com>
2025-06-26 23:19:04 +0100

    logodarkandwhite

--- meu_horario/resources/views/vendor/filament-panels/components/logo.blade.php
+++ meu_horario/resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,5 +1,29 @@
+<style>
+    .logo-light {
+        display: block;
+    }
+
+    .logo-dark {
+        display: none;
+    }
+
+    .dark .logo-light,
+    [data-theme="dark"] .logo-light,
+    html.dark .logo-light {
+        display: none !important;
+    }
+
+    .dark .logo-dark,
+    [data-theme="dark"] .logo-dark,
+    html.dark .logo-dark {
+        display: block !important;
+    }
+</style>
+
 @if (request()->is('meuhorario/login'))
-<img src="{{ asset('images/logo-login.png') }}" alt="meuHorario Login" style="height: 200px;">
+<img src="{{ asset('images/logo-login.png') }}" alt="meuHorario Login" class="logo-light" style="height: 200px;">
+<img src="{{ asset('images/logo-login-dark.png') }}" alt="meuHorario Login Dark" class="logo-dark" style="height: 200px;">
 @else
-<img src="{{ asset('images/logo-painel1.png') }}" alt="meuHorario Painel" style="height: 60px;">
+<img src="{{ asset('images/logo-painel.png') }}" alt="meuHorario Painel" class="logo-light" style="height: 60px;">
+<img src="{{ asset('images/logo-painel-dark.png') }}" alt="meuHorario Painel Dark" class="logo-dark" style="height: 60px;">
 @endif
